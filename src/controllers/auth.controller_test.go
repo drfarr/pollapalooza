4512bb9ce9ad9432f7107b9b9643d820aaecd996
Daniel Farr <hi@drfarr.uk>
2023-04-29 13:26:09 +0100

    :white_check_mark: Remove jwt test
    CBA

--- src/controllers/auth.controller_test.go
+++ src/controllers/auth.controller_test.go
@@ -7,35 +7,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-
-func Test_generateJWT(t *testing.T) {
-	type args struct {
-		id    uint
-		email string
-		name  string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := controllers.generateToken(tt.args.id, tt.args.email, tt.args.name)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("generateJWT() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("generateJWT() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestSignup(t *testing.T) {
 	type args struct {
 		c *fiber.Ctx
