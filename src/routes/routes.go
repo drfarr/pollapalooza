f09a0c69c3311c1e18efd7f2ad490a0983fee74e
Daniel Farr <hi@drfarr.uk>
2023-04-29 13:25:13 +0100

    :necktie: Add poll controllers to router
    Yay!

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -12,4 +12,13 @@ func Setup(app *fiber.App) {
 	admin.Post("signup", controllers.Signup)
 	admin.Post("signin", controllers.Signin)
 	admin.Get("user", controllers.User)
+
+	// TODO: Middleware these suckas for user auth policy
+	poll := api.Group("poll")
+	poll.Get("/", controllers.ListPolls)
+	poll.Get("/:id", controllers.GetPoll)
+	poll.Post("/", controllers.CreatePoll)
+	poll.Put("/:id", controllers.UpdatePoll)
+	poll.Delete("/:id", controllers.DeletePoll)
+	poll.Post("/:id/vote", controllers.RegisterVote)
 }
