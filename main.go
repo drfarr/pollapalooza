b7ca84b38472335e3f0689f6bddab714cfe61162
Daniel Farr <hi@drfarr.uk>
2023-05-10 13:42:39 +0100

    :art: Add config and update db connection

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"log"
 	"pollapalooza/src/database"
 	"pollapalooza/src/routes"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	database.Connect()
-	database.AutoMigrate()
+
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
+	}))
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
 	}))
 
 	routes.Setup(app)
--- src/database/db.go
+++ src/database/db.go
@@ -1,44 +1,30 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"pollapalooza/src/models"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-// use godot package to load/read the .env file and
-// return the value of the key
-func goDotEnvVariable(key string) string {
-
-	// load .env file
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	return os.Getenv(key)
-}
-
 func Connect() {
 
 	var err error
-	dsn := goDotEnvVariable("DATABASE_URL")
+	dsn := os.Getenv("DATABASE_URL")
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	_ = DB
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-}
+	fmt.Println("Connection Opened to Database")
+	DB.AutoMigrate(&models.Poll{}, &models.User{}, &models.Session{})
+	fmt.Println("Database Migrated")
 
-func AutoMigrate() {
-	DB.AutoMigrate(models.User{})
 }
