e3016da7f41c47ccc8377e94c6e2cdf60b446934
Daniel Farr <hi@drfarr.uk>
2023-05-10 13:45:35 +0100

    :white_check_mark: Adding auth controller test template

--- src/controllers/auth.controller_test.go
+++ src/controllers/auth.controller_test.go
@@ -20,7 +20,7 @@ func TestSignup(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := controllers.Signup(tt.args.c); (err != nil) != tt.wantErr {
+			if err := controllers.SignUp(tt.args.c); (err != nil) != tt.wantErr {
 				t.Errorf("Signup() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -40,8 +40,28 @@ func TestSignin(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := controllers.Signin(tt.args.c); (err != nil) != tt.wantErr {
+			if err := controllers.SignIn(tt.args.c); (err != nil) != tt.wantErr {
 				t.Errorf("Signin() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestSignOut(t *testing.T) {
+	type args struct {
+		c *fiber.Ctx
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SignOut(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("SignOut() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
